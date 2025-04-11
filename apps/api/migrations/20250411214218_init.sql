43919098a70501c81bd5fae2de52d7d4fd3f7fa5
Lawand Othman <lwnd@pm.me>
2025-04-11 23:03:18 +0100

    db

---
+++ apps/api/migrations/20250411214218_init.sql
@@ -0,0 +1,27 @@
+-- Add migration script here
+
+CREATE TABLE submissions (
+  id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+  name TEXT NOT NULL,
+  start_time BIGINT NOT NULL,
+  end_time BIGINT,
+  created_at TIMESTAMPTZ DEFAULT NOW(),
+  )
+
+  CREATE OR REPLACE FUNCTION get_submission_score(start_time BIGINT, end_time BIGINT) 
+RETURNS BIGINT AS $$
+BEGIN
+RETURN CASE
+WHEN end_time IS NULL THEN NULL
+ELSE end_time - start_time
+  END;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE VIEW leaderboard AS
+SELECT id, name, start_time, end_time, get_submission_score(start_time, end_time) AS score
+FROM submissions
+WHERE end_time IS NOT NULL
+ORDER BY 
+get_submission_score(start_time, end_time) ASC;
+
--- apps/api/src/main.rs
+++ apps/api/src/main.rs
@@ -1,9 +1,31 @@
+use std::{env, net::SocketAddr};
+
 use axum::{Router, routing::get};
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .min_connections(2)
+        .connect(&database_url)
+        .await?;
+
+    sqlx::migrate!("./migrations").run(&pool).await?;
+
     let app = Router::new().route("/", get(|| async { "Hello from fly.io!" }));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+    let port = env::var("PORT").unwrap_or_else(|_| "8080".to_string());
+    let addr = format!("0.0.0.0:{}", port).parse::<SocketAddr>()?;
+
+    println!("🚀 Listening on http://{}", addr);
+
+    axum::serve(tokio::net::TcpListener::bind(addr).await?, app).await?;
+
+    Ok(())
 }
