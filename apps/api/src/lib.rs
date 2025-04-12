812f8569f8d5ccbb53e3b6ed72d85e62c6718819
Lawand Othman <lwnd@pm.me>
2025-04-12 19:14:34 +0100

    migrations

--- apps/api/src/lib.rs
+++ apps/api/src/lib.rs
@@ -1,5 +1,6 @@
 use crate::error::AppError;
 use axum::{Router, routing::get};
+use migration::{Migrator, MigratorTrait};
 
 pub mod config;
 pub mod db;
@@ -7,6 +8,8 @@ pub mod error;
 
 pub async fn run_app() -> Result<Router, AppError> {
     let connection = db::connect().await?;
+
+    Migrator::up(&connection, None).await?;
 
     let app = Router::new().route("/", get(|| async { "Hello from fly.io!" }));
 
