85d3f1674afc1cf8ce9d7ee2010be1ba5b831946
Lawand Othman <lwnd@pm.me>
2025-04-12 13:16:57 +0100

    fix

--- apps/api/migration/src/m20220101_000001_create_table.rs
+++ apps/api/migration/src/m20220101_000001_create_table.rs
@@ -1,4 +1,4 @@
-use sea_orm_migration::{prelude::*, schema::*};
+use sea_orm_migration::prelude::*;
 
 #[derive(DeriveMigrationName)]
 pub struct Migration;
@@ -6,36 +6,41 @@ pub struct Migration;
 #[async_trait::async_trait]
 impl MigrationTrait for Migration {
     async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
             .create_table(
                 Table::create()
-                    .table(Post::Table)
+                    .table(Submission::Table)
                     .if_not_exists()
-                    .col(pk_auto(Post::Id))
-                    .col(string(Post::Title))
-                    .col(string(Post::Text))
+                    .col(
+                        ColumnDef::new(Submission::Id)
+                            .uuid()
+                            .not_null()
+                            .primary_key(),
+                    )
+                    .col(ColumnDef::new(Submission::Name).string().not_null())
+                    .col(
+                        ColumnDef::new(Submission::StartTime)
+                            .big_integer()
+                            .not_null(),
+                    )
+                    .col(ColumnDef::new(Submission::EndTime).big_integer().null())
                     .to_owned(),
             )
             .await
     }
 
     async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
-        // Replace the sample below with your own migration scripts
-        todo!();
-
         manager
-            .drop_table(Table::drop().table(Post::Table).to_owned())
+            .drop_table(Table::drop().table(Submission::Table).to_owned())
             .await
     }
 }
 
 #[derive(DeriveIden)]
-enum Post {
+enum Submission {
     Table,
     Id,
-    Title,
-    Text,
+    Name,
+    StartTime,
+    EndTime,
 }
